65528bed1d71107d0129841897e40ce44e07ed6d
Sergiorodp <sergio.rod.perez@outlook.es>
2021-01-11 15:43:43 -0500

    main page with nav bar

---
+++ components/navbar/index.js
@@ -0,0 +1,62 @@
+// import navStyle from '../../../styles/NavStyle/Nav.module.css'
+import styles from './styles'
+import Link from 'next/link'
+import { createRef } from 'react'
+
+const lineUp = createRef()
+const LineDown = createRef()
+const itemsContainer = createRef()
+const listItems = createRef()
+
+export default function NavBar({bodyRef}){
+    
+    var visible = false
+
+    const show = () =>{
+        visible = !visible
+        if(visible){
+            // console.log('abierto')
+            document.body.classList.add("body-overflow")
+            lineUp.current.classList.add("moverLeft")
+            LineDown.current.classList.add("moverRight")
+            itemsContainer.current.classList.add("opciones-open")
+            listItems.current.classList.add("listItems-open")
+        }else{
+            // console.log('cerrado')
+            document.body.classList.remove("body-overflow")
+            lineUp.current.classList.remove("moverLeft")
+            LineDown.current.classList.remove("moverRight")
+            itemsContainer.current.classList.remove("opciones-open")
+            listItems.current.classList.remove("listItems-open")
+        }
+    }
+
+    return(
+        <>
+        <div className= "mainNavContainer">
+            <div className= "navItemsContainer">
+                <h2>Stella Perez</h2>
+                
+                <div className = "opciones" ref = {itemsContainer} onClick = {show}>
+                    <ul className = "listItems" ref = {listItems}>
+                        <li> <Link href='/'> Inicio </Link></li>    
+                        <li><Link href='/api/hello'>Sobre mí</Link></li>
+                        <li><Link href='https://vercel.com/dashboard/integrations'>Servicios</Link></li>
+                        <li><Link href='https://vercel.com/dashboard/integrations'>Contacto</Link></li>
+                    </ul>
+                </div>
+
+                <div className = "lines-nav-container">
+                    <div className = "lines-block" onClick = {show}>
+                        <div className = "line-one line-div " ref = {lineUp} ></div>
+                        <div className = "line-two line-div" ></div>
+                        <div className = "line-tree line-div " ref={LineDown} ></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <style jsx>{styles}
+        </style>
+        </>
+    )
+}
---
+++ components/navbar/styles.js
@@ -0,0 +1,140 @@
+import css from 'styled-jsx/css'
+import { colors, breackPoint } from '../appLayout/index'
+
+export default css`
+
+.mainNavContainer{
+    z-index: 1100;
+    position: fixed;
+    width: 100vw;
+    background: ${colors.primary};
+    display: grid;
+    height: 4rem;
+    place-content: center;
+}
+
+.navItemsContainer{
+    color: ${colors.white};
+    max-width: 1600px;
+    width: 70vw;
+    /* margin: 0 20%; */
+    display: grid;
+    /* background: red; */
+    grid-template-columns: repeat(5,1fr);
+}
+
+.navItemsContainer p{
+    display: flex;
+    flex-direction: column;
+    /* background: red; */
+    align-items: center;
+    justify-content: center;
+}
+
+.body-active{
+  overflow: hidden;
+}
+.opciones{
+    grid-column: 3/6;
+    display: flex;
+    justify-content: center;
+    /* background: red; */
+}
+
+.listItems{
+    display: flex;
+    list-style: none;
+    align-items: center;
+    justify-content: center;
+}
+
+.listItems li{
+    font-weight: 500;
+    margin: 0 20px;
+}
+
+.listItems li :hover{
+    color: ${colors.secondary};
+    /* color: #F2811D; */
+}
+
+.lines-nav-container{
+    display: none;
+}
+
+.lines-block{
+    cursor: pointer;
+}
+
+@media (max-width: ${breackPoint.mobile}){
+
+    .navItemsContainer{
+        width:80vw;
+        grid-template-columns: repeat(2,1fr);
+    }
+    
+    .lines-nav-container{
+        display: flex;
+        grid-column: 2/3;
+        align-items: center;
+        /* background:black; */
+    }
+
+    .lines-block{
+        margin:0 0 0 auto;
+    }
+
+    .line-div{
+        width: 25px;
+        height: 2px;
+        background-color: ${colors.white};
+        margin-top: 4px;
+        margin-bottom: 4px;
+        border-radius:9999px;
+        transition: 0.2s ease-in-out;
+    }
+
+    .moverLeft{
+        width: 35px;
+    }
+
+    .moverRight{
+        width: 28px;
+    }
+
+
+    .opciones{
+        height: 100vh;
+        width: 105vw;
+        margin: 46px 0 0 0;
+        position:fixed;
+        background-color: rgba(0 ,0 ,0 ,0);
+        overflow: scroll;
+        transform: translateX(100%);
+        transition: transform .6s ease-in-out;
+    }
+
+    .listItems{
+        background:${colors.primary};
+        margin: 0 0 0 7em;
+        width: 100vw;
+        transform: translateX(100%);
+        transition: transform .6s ease-in-out;
+    }
+
+    .listItems-open{
+        transform: translateX(0%);
+    }
+
+    .opciones-open{
+        background-color: rgba(0 ,0 ,0 ,0);
+        transform: translateX(-10%);
+    }
+
+    .listItems{
+        flex-direction: column;
+    }
+
+}
+
+`
